f36a2878efc23ba367a9c4cd2eb801e52835966b
Julio09Angel <78927044+Julio09Angel@users.noreply.github.com>
2021-02-21 19:10:41 -0600

    Add files via upload

---
+++ Exercise1-2.cpp
@@ -0,0 +1,9 @@
+//Experiment to find out what happens 
+//when prints' argument 
+//string contains \c,
+//where c is a some character not listed above.
+#include<stdio.h>
+main()
+{
+	printf("hello, world\c");
+}
